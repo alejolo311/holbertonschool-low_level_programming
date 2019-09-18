8f516bf21e74d70d74eb7dac193a80b67b4578c5
alejolo311 <1147@holbertonschool.com>
2019-09-18 13:48:15 +0000

    write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+i
+int main(void)
+{
+	int string_lenght = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+
+	write(2,
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	      string_lenght);
+	return 1;
+}
