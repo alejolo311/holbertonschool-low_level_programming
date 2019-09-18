4e8f59d2736942b40c62290af9ba53a2ce6e030e
alejolo311 <1147@holbertonschool.com>
2019-09-18 19:49:55 +0000

    betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
-	int string_lenght = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	int l = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n",string_lenght);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	 l);
 
-	return 1;
+	return (1);
 }
