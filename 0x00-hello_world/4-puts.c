3fde916013275091490323cca40d6238f70e7763
alejolo311 <1147@holbertonschool.com>
2019-09-18 18:12:44 +0000

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
-*/
+ * main - entry
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
