2749e8b58c740dca987a2d99732f810886f5e4c1
alejolo311 <1147@holbertonschool.com>
2019-09-18 20:12:03 +0000

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 #include <string.h>
 /**
-  * main - Entry point
-  *
-  * Return: Always 0 (Success)
-  */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int l = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
