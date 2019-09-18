073d4ff14a7751c64a8166ca06fddc705bd98034
alejolo311 <1147@holbertonschool.com>
2019-09-18 20:04:00 +0000

    fix escape quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,10 +8,9 @@
   */
 int main(void)
 {
-	int l = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	int l = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 	 l);
-
 	return (1);
 }
