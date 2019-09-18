239a830c0f46da10c5780913aad3d9715ee45d26
alejolo311 <1147@holbertonschool.com>
2019-09-18 14:15:15 +0000

    fix the identation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main () {
-    puts("\"Programming is like building a multilingual puzzle");	
-    return 0;
+int main (void) {
+	puts("\"Programming is like building a multilingual puzzle");	
+	return 0;
 }
