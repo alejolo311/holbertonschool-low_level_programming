bb72adb517edb0d70f90052c0ebd518386645a47
alejolo311 <1147@holbertonschool.com>
2019-09-19 15:48:43 +0000

    numbers again, task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Desc: Use `putchar` function to combinations of numbers
+ * Return: 0
+ */
+int main(void)
+{
+	char number = '0';
+
+	while (number <= '9')
+	{
+		putchar(number);
+		if (number < '9')
+		{
+		putchar(44);
+		putchar(32);
+		}
+		number++;
+	}
+	putchar('\n');
+
+	return (0);
+}
