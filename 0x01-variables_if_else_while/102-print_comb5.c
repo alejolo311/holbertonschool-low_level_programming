7dec0f88079a21d1acfd7c7fe4158a65dccad2f1
alejolo311 <1147@holbertonschool.com>
2019-09-19 23:11:07 +0000

    at last

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Desc: Use `putchar` function to print number from 0001 to 9999
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+	int q, w, e, r;
+
+	for (i = 0; i < 100; i++)
+	{
+		q = i / 10; /* doubles fnum */
+		w = i % 10; /* singles fnum */
+
+		for (j = 0; j < 100; j++)
+		{
+			e = j / 10; /* doubles snum */
+			r = j % 10; /* singles snum */
+
+			if (q < e || (q == e && w < r))
+			{
+				putchar(q + '0');
+				putchar(w + '0');
+				putchar(32);
+				putchar(e + '0');
+				putchar(r + '0');
+
+				if (!(q == 9 && w == 8))
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
