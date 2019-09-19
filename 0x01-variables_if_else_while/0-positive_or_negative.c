5b0b430c0cf366307d75ec11eee085b5e2cc7b93
alejolo311 <1147@holbertonschool.com>
2019-09-19 17:30:05 +0000

    entrypont

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ * Desc: Use `putchar` function to print the alphabet.
+ * Return: 0
+*/
 int main(void)
 {
 	int n;
