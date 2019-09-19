0fbda8e6c034c3690991e6f927ab7df42cc25629
alejolo311 <1147@holbertonschool.com>
2019-09-19 15:37:47 +0000

    hexa

---
+++ 0x01-variables_if_else_while/8-print_nase16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Desc: Use `putchar` function to print the numbers in base 16.
+ * Return: 0
+ */
+int main(void)
+{
+	int number = '1';
+	char letter = 'a';
+
+	while (number <= '9')
+	{
+		putchar(number);
+		number++;
+	}
+	while (letter <= 'f')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
