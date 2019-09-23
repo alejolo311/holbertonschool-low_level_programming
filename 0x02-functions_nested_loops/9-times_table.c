137918041272c0396b2c1ac92291a57de7c6e952
alejolo311 <1147@holbertonschool.com>
2019-09-23 23:55:32 +0000

    a complete mess

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,25 +1,51 @@
 #include "holberton.h"
 /**
- * jack_bauer - This functions prints the longest day of jack baur
+ * times_table - This functions prints the longest day of jack baur
 (* a blank line
  * Description: this function print the longest day of jack bauer)?
 (* section header: the header file for this function is holberton.h)*
  * Return: no return a void function.
  */
-void  jack_bauer(void)
+void  times_table(void)
 {
-	int hours, minutes;
+	int table, number;
 
-	for (hours = 0; hours <= 23; hours++)
+	for (table = 0; table <= 9; table++)
 	{
-		for (minutes = 0; minutes < 60; minutes++)
+		for (number = 0; number <= 9; number++)
 		{
-			_putchar((hours / 10) + '0');
-			_putchar((hours % 10) + '0');
-			_putchar(':');
-			_putchar((minutes / 10) + '0');
-			_putchar((minutes % 10) + '0');
-			_putchar('\n');
+			int r;
+
+			r = table * number;
+			if (r >= 10)
+			{
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
+			}
+			else
+			{
+				_putchar(r + '0');
+			}
+			if ( number == 9)
+			{
+				;
+			}
+			else if (((r * 1.5) >= 10) && number != 5)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (number == 5 && (r*2) > 10)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
 	}
 }
