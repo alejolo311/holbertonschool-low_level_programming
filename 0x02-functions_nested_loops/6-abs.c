716b4b8be510ce2bb3754c6d5f895cba71d925da
alejolo311 <1147@holbertonschool.com>
2019-09-23 17:35:42 +0000

    intenger

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _abs - This functions prints the absolute value of a integer
+(* a blank line
+ * Description: this function print the absolute value of a integer)?
+(* section header: the header file for this function is holberton.h)*
+ * Return: return  the absolute value of a integer
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int r;
+
+		r = c * -1;
+		return (r);
+	}
+	else
+	{
+		return (c);
+	}
+}
