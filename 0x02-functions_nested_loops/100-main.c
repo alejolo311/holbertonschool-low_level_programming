77697d3da8d3074b3119d28e06b0f147cb8d89bb
alejolo311 <1147@holbertonschool.com>
2019-09-24 14:18:48 +0000

    fix the if condition on the function

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	print_times_table(3);
+	print_times_table(0);
 	_putchar('\n');
 	print_times_table(5);
 	_putchar('\n');
--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,7 +29,7 @@ void print_times_table(int n)
 {
 	int table, number;
 
-	if (n > 0 && n < 15)
+	if (n > 0 && n <= 15)
 	{
 	for (table = 0; table <= n; table++)
 	{
