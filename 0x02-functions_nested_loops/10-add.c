c5ef7efc5d8daec4591db9be68e005f8005ee0c3
alejolo311 <1147@holbertonschool.com>
2019-09-23 20:32:40 -0500

    task 10, add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * add - This functions adds two numbers
+(* a blank line
+ * Description: this function print the add og two numbers)?
+(* section header: the header file for this function is holberton.h)*
+ * Return: retunrn the add result of two numbers
+ */
+int  add(int n, int n2)
+{
+	int result;
+
+	result = n + n2;
+
+	return (result);
+}
