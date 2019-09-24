c4be3233c524c530845ec56da03c75287c5ecfe1
alejolo311 <1147@holbertonschool.com>
2019-09-24 18:19:54 +0000

    try

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,12 +12,12 @@ int main(void)
 	fib1 = 0;
 	fib2 = 1;
 
-	for (i = 0; i <= 50; i++)
+	for (i = 0; i < 50; i++)
 	{
 		fib = fib1 + fib2;
 		fib1 = fib2;
 		fib2 = fib;
-		if (i != 50)
+		if (i != 49)
 			printf("%ld, ", fib);
 		else
 			printf("%ld\n", fib);
