b940df394a31b32fc8420f1d3d5d2f8bc88a7fd7
alejolo311 <1147@holbertonschool.com>
2019-09-24 12:15:13 +0000

    fix betty

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 /**
  * _isalpha - This functions says if a characters is alpha
+ * @c: this parameters is a integer
 (* a blank line
  * Description: this function detect if a charecter is alpha)?
 (* section header: the header file for this function is holberton.h)*
- * Return: return 1 if the value is lower or alpha, return 0 in other cases.
+ * Return: return 1 if the value is lower or alpha, return 0
  */
 int _isalpha(int c)
 {
