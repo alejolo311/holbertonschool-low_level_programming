b9506ff486e6847dfb7f336f1351bb79e9f9bc5d
alejolo311 <1147@holbertonschool.com>
2019-09-23 21:18:42 -0500

    task 11 print_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - This functions prints numbers to 98
+(* a blank line
+ * Description: this function print numbers to 98)?
+(* section header: the header file for this function is holberton.h)*
+ * Return: this is a void function, dont have return.
+ */
+void  print_to_98(int c)
+{
+	if (c > 98)
+	{
+		while (c >= 98)
+		{
+			printf("%i", c);
+			if (c != 98)
+			{
+				printf(", ");
+			}
+			c--;
+		}
+	}
+	else
+	{
+		while (c <= 98)
+		{
+			printf("%i", c);
+			if (c != 98)
+			{
+				printf(", ");
+			}
+			c++;
+		}
+	}
+	printf("\n");
+}
