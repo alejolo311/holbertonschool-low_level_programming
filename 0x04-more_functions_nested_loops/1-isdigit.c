cfdaf6792aecd352159c4388025a547211624569
alejolo311 <1147@holbertonschool.com>
2019-09-26 12:58:23 +0000

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+/**
+ * _isdigit - this function says if a character is a digit
+ * @c: the character to be process
+(* a blank line
+* Description: this function says if a character is a digit)?
+(* section header: the header of this function is holberton.h)*
+* Return: return 1 if c is a digit and 0 in other cases.
+*/
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+		return (1);
+	else
+		return (0);
+}
