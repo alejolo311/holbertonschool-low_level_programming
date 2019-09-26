7caa13c1eb43cc0db81bb36d1314c4de314ab193
alejolo311 <1147@holbertonschool.com>
2019-09-26 20:37:36 +0000

    fix the add before print

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -14,17 +14,18 @@ int main(void)
 	j = 612852475143;
 
 	i = 2;
-	while( j != 1)
+	while (j != 1)
 	{
 		if (j % i == 0)
 		{
-			while ( j % i == 0)
+			while (j % i == 0)
 			{
 				j = j / i;
 			}
 		}
 		i++;
 	}
+	i--;
 	printf("%d\n", i);
 	return (0);
 }
