1fe4b107ed80872abe8efcd713f696d7f2ebb2af
alejolo311 <1147@holbertonschool.com>
2019-09-26 13:03:14 +0000

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul - this function multiply two digits
+ * @a: the first character to be process
+ * @b: the second character
+(* a blank line
+* Description: this function multiply two numbers)?
+(* section header: the header of this function is holberton.h)*
+* Return: return the multyply of two numbers
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
