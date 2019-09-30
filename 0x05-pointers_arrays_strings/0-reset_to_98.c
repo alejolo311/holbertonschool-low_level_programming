ab47ae430171de202e3020b6e813e8497c591681
alejolo311 <1147@holbertonschool.com>
2019-09-30 13:51:31 +0000

    create the folder, the readme and the first file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - this function reset a variable to 98
+(* a blank line
+* Description: this function reset the value of a variable to 98)?
+(* section header: the header of this function is holberton.h)*
+* Return: a void function, no return
+*/
+void reset_to_98(int *n)
+{
+
+
+}
