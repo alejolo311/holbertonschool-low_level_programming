16428d85c7c62e5c89f76454f2968dc59cb5b15a
alejolo311 <1147@holbertonschool.com>
2019-10-01 15:54:19 +0000

    print an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * print_array - this function prints a str only the even lines
+(* a blank line
+ *@a: this parameter is the arr to ´print
+ *@n: the number of elements to print
+* Description: this funtion prints a str only the even lines)?
+(* section header: the header of this function is holberton.h)*
+* Return: this is a void funtion no return.
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i <= n)
+	{
+		printf("%d", a[i]);
+		if (i == n)
+			printf("\n");
+		else
+		{
+			printf(", ");
+		}
+		i++;
+	}
+
+}
