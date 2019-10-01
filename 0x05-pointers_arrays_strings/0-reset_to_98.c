72997e5a63ccbea673e8d518b5a8bb61d657afe3
alejolo311 <1147@holbertonschool.com>
2019-10-01 12:42:36 +0000

    fix the doc style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,6 +2,7 @@
 /**
  * reset_to_98 - this function reset a variable to 98
 (* a blank line
+ *@n: this parameters is a pointer to a variable in main.
 * Description: this function reset the value of a variable to 98)?
 (* section header: the header of this function is holberton.h)*
 * Return: a void function, no return
