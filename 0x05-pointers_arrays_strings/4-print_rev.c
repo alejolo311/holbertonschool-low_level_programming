9857947af621e61ff886d8bfcd3b03216a7a8c06
alejolo311 <1147@holbertonschool.com>
2019-10-01 13:24:51 +0000

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * print_rev - this function prints a str in rev
+(* a blank line
+ *@s: this parameter is the str
+* Description: this funtion prints a str in rev)?
+(* section header: the header of this function is holberton.h)*
+* Return: this is a void funtion no return.
+*/
+void print_rev(char *s)
+{
+	int size;
+
+	size = 0;
+
+	while (s[size] != '\0')
+	{
+		size++;
+	}
+
+	while (s[size] >= 0)
+	{
+		_putchar(s[size]);
+		size--;
+
+		if (size < 0)
+			_putchar('\n');
+	}
+
+}
