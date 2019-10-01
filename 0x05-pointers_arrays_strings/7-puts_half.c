a7f21636534bbacab129ea9ef6fb06eaa0d150f0
alejolo311 <1147@holbertonschool.com>
2019-10-01 15:05:12 +0000

    fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- * puts2 - this function prints a str only the even lines
+ * puts_half - this function prints a str only the second half part
 (* a blank line
  *@str: this parameter is the str
-* Description: this funtion prints a str only the even lines)?
+* Description: this funtion prints a str only the second half part)?
 (* section header: the header of this function is holberton.h)*
 * Return: this is a void funtion no return.
 */
@@ -13,9 +13,6 @@ void puts_half(char *str)
 
 	for (size = 0; str[size] != '\0'; size++)
 		;
-
-	if (size % 2 != 0 )
-		size--;
 
 	for (size /= 2; size != '\n'; size++)
 	{
