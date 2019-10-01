3c52dec03c2f92e4a688c1f277567c9bf26dcf14
alejolo311 <1147@holbertonschool.com>
2019-10-01 15:19:59 +0000

    fix the odds logic again

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,8 +13,11 @@ void puts_half(char *str)
 
 	for (size = 0; str[size] != '\0'; size++)
 		;
-	size++;
-	for (size /= 2; size != '\n'; size++)
+	if ( size % 2 != 0)
+	{
+		size--;
+	}
+	for (size /= 2; str[size] != '\0'; size++)
 	{
 		_putchar(str[size]);
 	}
