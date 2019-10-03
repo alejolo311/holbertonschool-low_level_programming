f2ea6bec974b62780398a6249863e609d4516e1a
alejolo311 <1147@holbertonschool.com>
2019-10-03 19:44:17 +0000

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -35,19 +35,11 @@ char *cap_string(char *a)
 
 	for (j = 0; a[j] != '\0'; j++)
 	{
-		if (isaspace(a[j -1]))
+		if (isaspace(a[j - 1]))
 		{
 			if (a[j] > 96 && a[j] < 123)
 			{
 				a[j] -= 32;
-			}
-			else if (a[j] > 64 && a[j] < 91)
-			{
-				a[j] = a[j];
-			}
-			else if (a[j] > 47 && a[j] < 58)
-			{
-				a[j] = a[j];
 			}
 		}
 	}
