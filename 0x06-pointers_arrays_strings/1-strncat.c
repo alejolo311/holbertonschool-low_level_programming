69b2736fee8c674eeaf122a5854e1b0ff3d837c5
alejolo311 <1147@holbertonschool.com>
2019-10-03 14:43:50 +0000

    concatenate two strings with a number specific of bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * _strcat - this function concatenate two strings
+ * @dest: the first string to be concatenated
+ * @src: the second string to be concatenated
+ * @n: the number of bytes to concatenate
+(* a blank line
+* Description: this function concatenate strings using n as number of bytes)?
+(* section header: the header of this function is holberton.h)*
+* Return: return the final result of the string in char
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while(dest[i] != '\0')
+	{
+		i++;
+	}
+
+	for(j = 0; src[j] != '\0'; j++, i++)
+	{
+		j < n ? dest[i] = src[j] : j;
+	}
+
+
+	return (dest);
+}
