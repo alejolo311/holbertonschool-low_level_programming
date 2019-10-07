1d210c56f8e723b02c9d33f141b77be315800c59
alejolo311 <1147@holbertonschool.com>
2019-10-07 22:30:19 +0000

    task 9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * set_string - this function set a pointer to pointer
+ * @s: the pointer
+ * @to: the other pointer
+ (* a blank line
+* Description: this function set a pointer)?
+(* section header: the header of this function is holberton.h)*
+* Return: no return
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
