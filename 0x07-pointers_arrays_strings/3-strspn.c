755689b545374fe0788b08060b7aea320b13a63f
alejolo311 <1147@holbertonschool.com>
2019-10-07 18:52:31 +0000

    3 task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,14 +10,22 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
+	int i, j, k;
 
-	i = 0, j = 0;
+	k = 0;
 
-	while((*(s + i) = *(accept + j)) && *(s + i) != '\0')
+ 	for( i = 0; *(s + i) != '\0'; i++)
 	{
-		i++;
-		j++;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				k++;
+				break;
+			}
+		}
+		if (*(accept + j) == '\0')
+			break;
 	}
-	return (i);
+	return (k);
 }
