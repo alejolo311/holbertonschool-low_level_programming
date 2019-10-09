b639b6f5421bcc0a4cb04194d3170e0805e82899
alejolo311 <1147@holbertonschool.com>
2019-10-09 17:15:25 +0000

    fix betty

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,15 +1,18 @@
 #include "holberton.h"
 /**
- * factorial - this functions prints the factorial of a number
+ * _pow_recursion - this functions prints the factorial of a number
 (* a blank line
- *@n: this variable is a int , contains the number to be factorized
+ *@x: this variable is the number to be pow
+ *@y: the pow
 * Description: this function prints the factorial of a number?
 (* section header: the header of this function is holberton.h)*
 * Return: retunrn the factorial in int
 */
 int _pow_recursion(int x, int y)
 {
-	if (y == 0)  return (1);
-	if (y < 0)  return (-1);
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
 	return (x * _pow_recursion(x, y - 1));
 }
