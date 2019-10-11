78d26601efbfb6e1f51dbac0019da2eb943d4033
alejolo311 <1147@holbertonschool.com>
2019-10-11 17:21:39 +0000

    add the documentation

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,13 @@
 #include "holberton.h"
 #include <stdio.h>
+/**
+ * main - this program prints the number of elements
+(* a blank line
+*@argc: this is the quantity of parameters
+*@argv: the parameter to put in the program.
+* Description: this program prints the number of elements)?
+* Return: return 0 or 1
+ */
 int main(int argc, char *argv[])
 {
 	(void) argv;
