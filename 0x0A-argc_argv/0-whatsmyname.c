f3eda669689eb31d72808ec3b01a0765862febf7
alejolo311 <1147@holbertonschool.com>
2019-10-11 16:56:01 +0000

    fix doc and betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
-#include "holberton.h"
 #include <stdio.h>
-
+/**
+ * main - this program prints the argv[0]
+(* a blank line
+*@argc: this is the quantity of parameters
+*@argv: the parameter to put in the pogram.
+* Description: this program prints the argv[0])?
+* Return: return 0 or 1
+ */
 int main(int argc, char *argv[])
 {
 	(void) argc;
