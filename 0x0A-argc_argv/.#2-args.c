f466527512032b54e1be8d2f0b5cfa2bf3001578
alejolo311 <1147@holbertonschool.com>
2019-10-14 01:09:43 +0000

    fix betty and doc

---
+++ 0x0A-argc_argv/.#2-args.c
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.1731:1571014353
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,13 @@
 #include "holberton.h"
 #include <stdio.h>
+/**
+ * main - this program prints all the arguments
+(* a blank line
+*@argc: this is the quantity of parameters
+*@argv: the parameter to put in the program.
+* Description: this program prints all the arguments)?
+* Return: return 0 or 1
+ */
 
 int main(int argc, char *argv[])
 {
