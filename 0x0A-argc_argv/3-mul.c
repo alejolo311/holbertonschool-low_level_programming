5062d38139b5207ff7837ffe66e49d475d5dd3a0
alejolo311 <1147@holbertonschool.com>
2019-10-14 01:18:16 +0000

    fix doc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,15 @@
 #include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
+
+/**
+ * main - this program multuply two numbers
+(* a blank line
+*@argc: this is the quantity of parameters
+*@argv: the parameter to put in the program.
+* Description: this program multuply two numbers)?
+* Return: return 0 or 1
+ */
 int main(int argc, char *argv[])
 {
 	int result;
