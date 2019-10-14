0ecbc5335f88e08e13c8fd9f0af16291628aab99
alejolo311 <1147@holbertonschool.com>
2019-10-14 02:53:41 +0000

    fix some mistakes

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] > '9' && argv[i][j] < '0')
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
@@ -28,11 +28,42 @@ int main(int argc, char *argv[])
 
 		}
 
-		sum += atoi(argv[i]);
+		result += _atoi(argv[i]);
 
 	}
 
 	printf("%d\n", result);
 
 	return (0);
+}
+/**
+ * _atoi - this function converts a string to integer
+(* a blank line
+*@s: the string for convert to integer
+* Description: this funtion converts a string to integer)?
+(* section header: the header of this function is holberton.h)*
+* Return: this funtion returns the result
+*/
+int _atoi(char *s)
+{
+	int counter, sign;
+	unsigned int number;
+
+	sign = 1;
+	counter = 0;
+	number = 0;
+
+
+	while ((s[counter] < '0' || s[counter] > '9') && (s[counter] != '\0'))
+	{
+		if (s[counter] == '-')
+			sign *= -1;
+		counter++;
+	}
+	while ((s[counter] >= '0') && (s[counter] <= '9'))
+	{
+		number = number * 10 + (s[counter] - '0');
+		counter++;
+	}
+	return (number * sign);
 }
--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 
 int _putchar(char c);
+int _atoi(char *s);
 
 #endif /* HOLBERTON_H */
