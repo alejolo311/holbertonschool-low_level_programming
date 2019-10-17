42a60fb0d9995bd9cbc4bd8492f3482a8b2c1125
alejolo311 <1147@holbertonschool.com>
2019-10-17 18:25:00 +0000

    task 1 copy a string

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * string_nconcat - copy a string.
+(* a blank line
+ *@s1: the first string
+ *@s2: the second string
+ *@n: the number of bytes
+* Description: copy a string)?
+(* section header: the header of this function is holberton.h)*
+* Return: returns a pointer to a char..
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	unsigned int i, j, k;
+
+
+	i = 0;
+	s1 == NULL ? s1 = "" : s1;
+	s1 == NULL ? s2 = "" : s2;
+	while (*(s1 + i) != '\0')
+		i++;
+
+
+	p = malloc((i + n + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (j = 0, k = 0; j < (i + n); j++)
+	{
+		if (j < i)
+			p[j] = s1[j];
+		else
+			p[j] = s2[k++];
+	}
+	p[j] = '\0';
+
+	return (p);
+}
