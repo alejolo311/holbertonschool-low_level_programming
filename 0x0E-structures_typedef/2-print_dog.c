8a826f8a74339cf1606620f56ade0a3a02eb7f07
alejolo311 <1147@holbertonschool.com>
2019-10-21 15:08:52 +0000

    task 2 - this function prints a dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+/**
+ * print_dog - this function print a dog.
+ * @d: the struct of the dog
+ (* a blank line
+* Description: this function print a dog)?
+(* section header: the header of this function is dog.h)*
+* Return: a void function no return.
+*/
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d.name != NULL)
+			printf("Name: %s\n", d.name);
+		else
+			printf("Name: (nil)\n");
+		if (d.age != NULL)
+			printf("Age: %f\n", d.age);
+		else
+			printf("Age: (nil)\n");
+		if (d.owner != NULL)
+			printf("Owner: %s\n", d.owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+	else
+		return;
+}
