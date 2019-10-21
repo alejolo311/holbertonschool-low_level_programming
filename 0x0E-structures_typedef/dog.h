5dab5f21cb342e727a88afd0fcb66c2c698229ac
alejolo311 <1147@holbertonschool.com>
2019-10-21 15:12:13 +0000

    add the new typedef and the new functions

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,10 +17,13 @@ struct dog
 	char *owner;
 
 };
-
-
+/**
+ * dog_t - typedef for dog
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
