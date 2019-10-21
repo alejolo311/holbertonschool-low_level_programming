80379cdae2dfcdeaa1cf71a999d2a84e634e1818
alejolo311 <1147@holbertonschool.com>
2019-10-21 17:38:17 +0000

    retire the functions

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,18 +12,22 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i, j;
+	int i, j, k;
 	char *p, *q;
 
 	struct dog *new_dog;
+
+	i = 0; j = 0;
 
 	new_dog = malloc(sizeof(struct dog));
 
 	if (new_dog == NULL)
 		return (NULL);
 
-	i = _strlen(name);
-	j = _strlen(owner);
+	while (*(name + i) != '\0')
+		i++;
+	while (*(owner + j) != '\0')
+		j++;
 
 	p = malloc(sizeof(char) * i + 1);
 	if (p == NULL)
@@ -38,52 +42,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(p);
 		return (NULL);
 	}
-	_memcpy(p, name, i);
-	_memcpy(q, owner, j);
+	for (k = 0; k <= i; k++)
+		p[k] = name[k];
+	for (k = 0; k <= j; k++)
+		q[k] = owner[k];
 
 	new_dog->name = p;
 	new_dog->age = age;
 	new_dog->owner = q;
 
 	return (new_dog);
-}
-/**
- * _strlen - this functions prints the lenght of a string
-(* a blank line
-*@s: string to print.
-* Description: this function prints the lenght of a string?
-(* section header: the header of this function is dog.h)*
-* Return: retunrn the lenght in int
-*/
-int _strlen(char *s)
-{
-	if (*s != '\0')
-	{
-		s++;
-		return (1 + _strlen(s));
-	}
-	else
-		return (0);
-}
-/**
- * _memcpy - this function cpy a buffer
- * @dest: is a buffer that it contains in array
- * @src: the new value to copy in the buffer
- * @n: the number of bytes to be altered.
-(* a blank line
-* Description: this function set memory)?
-(* section header: the header of this function is holberton.h)*
-* Return: return char
-*/
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	int i;
-
-	for (i = 0; n > 0; i++, n--)
-	{
-
-		dest[i] = src[i];
-	}
-
-	return (dest);
 }
