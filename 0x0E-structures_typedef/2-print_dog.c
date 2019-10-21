39a71eca1a9bf5bb13759dc17323aadfb9f7ac90
alejolo311 <1147@holbertonschool.com>
2019-10-21 17:06:44 +0000

    fix the unnecesarry else

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,8 +16,6 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: (nil)\n");
 		printf("Age: %f\n", d->age);
-		else
-			printf("Age: (nil)\n");
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
