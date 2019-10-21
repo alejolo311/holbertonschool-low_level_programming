5d3f461fb7d782895b835ac9fc88b9140eac9974
alejolo311 <1147@holbertonschool.com>
2019-10-21 17:40:40 +0000

    fix the dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -25,7 +25,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-int _strlen(char *s);
-char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
