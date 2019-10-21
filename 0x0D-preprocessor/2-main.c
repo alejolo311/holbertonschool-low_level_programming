fa50b970c82b2011536660a7a02fd867f9fa14bd
alejolo311 <1147@holbertonschool.com>
2019-10-21 13:37:45 +0000

    task 2. print the name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+(* a blank line
+* Description: this program prints the name of the file)?
+* Return: return always 0
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
