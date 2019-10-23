058c138db024da7a43316b02bae2540564fe456d
alejolo311 <1147@holbertonschool.com>
2019-10-23 16:35:13 +0000

    create the functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,35 @@
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+int op_div(int a, int b)
+{
+	if (b != 0)
+		return ( a / b);
+	else
+	{
+		printf("Error\n");
+		exit(100);
+	}
+}
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
