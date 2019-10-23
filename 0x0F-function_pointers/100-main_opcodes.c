4439a7c09c617f16276fcb603cefa1eef037c83b
alejolo311 <1147@holbertonschool.com>
2019-10-23 16:34:55 +0000

    adv_task- print the opcodes in hex

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main- Entry point
+(* a blank line
+ *@argc: the number of parameters.
+ *@argv: the parameeters in the case the number ob bytes.
+* Description: this program prints opcodes in hexa)?
+* Return: 0 in succes
+*/
+int main(int argc, char *argv[])
+{
+	int i, n;
+
+	i = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	while (i < n)
+	{
+		printf("%02x", *((unsigned char *)main + i));
+		if (i < n)
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
