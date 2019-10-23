e13f0a1afc93c99ac4c6dc057228baae063925d4
alejolo311 <1147@holbertonschool.com>
2019-10-23 16:31:35 +0000

    task 2, find the index of a integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - this functions find the index of a int
+(* a blank line
+ *@array: the array to find the index
+ *@size: the size of the array
+ *@cmp: this a function pointer.
+* Description: this function find the index of a int)?
+(* section header: the header of this function is function_pointers.h)*
+* Return: this is a void function no return
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(*(array + i)))
+			return (i);
+	}
+
+	return (-1);
+}
