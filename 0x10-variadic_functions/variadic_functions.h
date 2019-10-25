1cdd924dbfc5d1b42705d31c0df5cc4ea29505bf
alejolo311 <1147@holbertonschool.com>
2019-10-25 16:24:32 +0000

    adding the header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,14 @@
+#ifndef VARIAFUNC
+#define VARIAFUNC
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* VARIAFUNC */
