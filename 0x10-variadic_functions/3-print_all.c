f93fc3f65be0e1f04e240951c019f9fc7c8afdf6
alejolo311 <1147@holbertonschool.com>
2019-10-28 04:50:23 +0000

    fix betty

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,11 +11,26 @@ void integer_print(va_list args)
 {
 	printf("%d", va_arg(args, int));
 }
-
+/**
+ * char_print - print chars.
+(* a blank line
+*@args: the list of parameters
+* Description: this function prints chars)?
+(* section header: the header of this function is variadic_functions.h)*
+* Return:no return a void func.
+ */
 void char_print(va_list args)
 {
 	printf("%c", va_arg(args, int));
 }
+/**
+ * string_print - print strings
+(* a blank line
+*@args: the list of parameters
+* Description: this function prints strings)?
+(* section header: the header of this function is variadic_functions.h)*
+* Return:no return a void func.
+*/
 
 void string_print(va_list args)
 {
@@ -26,6 +41,15 @@ void string_print(va_list args)
 		s = "(nil)";
 	printf("%s", s);
 }
+
+/**
+ * float_print - print floats.
+(* a blank line
+*@args: the list of parameters
+* Description: this function prints floats)?
+(* section header: the header of this function is variadic_functions.h)*
+* Return:no return a void func.
+ */
 void float_print(va_list args)
 {
 	printf("%f", va_arg(args, double));
