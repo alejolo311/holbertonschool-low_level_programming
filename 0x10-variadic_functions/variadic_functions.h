beecf595b7c7b91b744a863f8a75f50a99e0ccc7
alejolo311 <1147@holbertonschool.com>
2019-10-28 02:57:34 +0000

    fix the header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,14 +2,18 @@
 #define VARIAFUNC
 
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <strings.h>
+/**
+ * struct args_t - contain the type and the function.
+ * @format: the format to print
+ * @function: the functions that prints
+ *
+ * Description: contains the type and the format to print.
+ */
 typedef struct args_t
 {
 	char *format;
-	void (*function)(va_list);
+	void (*function)();
 } args_t;
 
 int sum_them_all(const unsigned int n, ...);
