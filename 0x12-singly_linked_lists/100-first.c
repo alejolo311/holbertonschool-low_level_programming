e7dca33eb3a7d5fc531942e94fe8479b4bfa6b96
alejolo311 <1147@holbertonschool.com>
2019-10-31 17:19:38 +0000

    fix the new ine

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,6 +2,6 @@
 
 void __attribute__ ((constructor)) premain(void)
 {
-	printf("You're beat! and yet, you must allow,");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
