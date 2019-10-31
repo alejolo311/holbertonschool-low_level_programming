a7b4b9a484c3ddd783794efa4c8340912da05c05
alejolo311 <1147@holbertonschool.com>
2019-10-31 17:21:54 +0000

    fix betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * premain - execute code before the main
+ * description - execute code before the main
+ * return - non return, void function
+ */
 void __attribute__ ((constructor)) premain(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
