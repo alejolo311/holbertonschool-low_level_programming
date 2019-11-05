cf1dd38d8324aaccf09a42e78e5f1c53435a001b
alejolo311 <1147@holbertonschool.com>
2019-11-05 22:49:30 +0000

    pop the lists

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * pop_listint - delete the head node
+(* a blank line
+*@head: the head of list
+* Description: this function delete the head note)?
+(* section header: the header of this function is lists.h)*
+* Return: void, no return
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *actual;
+	int result;
+
+	if (*head == NULL || head == NULL)
+		return (0);
+	actual = *head;
+	result = current->n;
+	*head = (*head)->next;
+	free(actual);
+	return (result);
+}
