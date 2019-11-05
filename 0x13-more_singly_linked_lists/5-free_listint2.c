19c32cc81fec72a7aeace1fa93b14f3d0e1f00b7
alejolo311 <1147@holbertonschool.com>
2019-11-05 22:39:03 +0000

    task 5 free again with doble pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * free_listint2 - free a list
+(* a blank line
+*@head: the head of list
+* Description: this function free a list.)?
+(* section header: the header of this function is lists.h)*
+* Return: void, no return
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *position, *actual;
+
+	if (head == NULL)
+		return;
+
+	actual = *head;
+	while (actual != NULL)
+	{
+		position = actual;
+		actual = postion->next;
+		free(position);
+	}
+	*head = '\0';
+	head = '\0';
+}
