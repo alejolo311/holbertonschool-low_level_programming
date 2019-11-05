9bd4cfeb20cc5a8fcd31f5a875e450830f2a8db1
alejolo311 <1147@holbertonschool.com>
2019-11-05 23:25:39 +0000

    add a new node in beetwen nodes

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - thsi function create a new index
+ * @head: the head of list
+ * @idx: the index to put the new node
+ * @n: the value to put in the nex node.
+ * Description: this function create a new index
+ * section header: the header of this function is lists.h)*
+ * Return: return the final result of the sum
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *node, *actual;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	node = malloc(sizeof(listint_t));
+
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+
+	if (idx == NULL)
+	{
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+
+	actual = *head;
+
+	for (i = 0; actual; i++)
+	{
+		if (i == idx - 1)
+		{
+			node->next = actual->next;
+			actual->next = node;
+			return (node);
+		}
+		current = current->next;
+	}
+	free(node);
+	return (NULL);
+}
