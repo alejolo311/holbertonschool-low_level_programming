f8d2c17234b79c9883ab1c4e18e931ce52f4508b
alejolo311 <1147@holbertonschool.com>
2019-11-06 03:54:14 +0000

    free in safe mode

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * free_listint_safe - thsi function free a list in safe mode
+ * @h: the head of list
+ * Description: this function free a string in a safe mode
+ * section header: the header of this function is lists.h)*
+ * Return: the size of the list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *tmp, *actual;
+	size_t i;
+
+	i = 0, actual = *h;
+	while (actual)
+	{
+		tmp = actual;
+		actual = actual->next;
+		free(tmp);
+		i++;
+
+		if (tmp < actual)
+			break;
+	}
+
+	*h = NULL;
+
+	return (i);
+}
