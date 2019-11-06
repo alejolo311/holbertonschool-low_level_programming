bfecee7c441cc0f452dcb55d8b6314339c1894e3
alejolo311 <1147@holbertonschool.com>
2019-11-06 01:48:00 +0000

    fix some things

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (head == NULL || *head == NULL)
 		return (-1);
 
-	current = *head;
+	actual = *head;
 
 	if (index == '\0')
 	{
