aff10b0c6835bddf7023d006c27cf3b1a9270e11
alejolo311 <1147@holbertonschool.com>
2019-11-06 02:53:01 +0000

    Reverse!

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+ * reverse_listint - thsi reverse a list
+ * @head: the head of list
+ * Description: this function delete a node
+ * section header: the header of this function is lists.h)*
+ * Return: 1 in success -1 in failure
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *tmp, *actual;
+
+	if (*head == NULL)
+		return (NULL);
+
+	actual = *head;
+	*head = actual->next;
+	tmp = (*head)->next;
+	actual->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = actual;
+		return (actual);
+	}
+
+	while (tmp)
+	{
+		(*head)->next = actual;
+		actual = *head;
+		*head = tmp;
+		tmp = (*head)->next;
+	}
+
+	(*head)->next = actual;
+	return (*head);
+}
