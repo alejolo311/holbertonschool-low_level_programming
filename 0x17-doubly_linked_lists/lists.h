3ef2643e4041db1de3438e5ff007f78d2f80ec66
Alejo López <alejolo311@gmail.com>
2019-12-13 09:07:15 -0500

    add the list to the header

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,6 +3,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
