abcd3a621711ce0989330003a7f44b07f405e8c7
Alejo López <alejolo311@gmail.com>
2019-12-13 08:42:30 -0500

    print a double list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint -
- *
- * Return: 
+ * print_dlistint - print a double linked list
+ * @h: the double linked list
+ * Return: the len of the double linked list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
+	const dlistint_t *actual = h;
+	size_t len = 0;
 
+	while (actual)
+	{
+		printf("%i\n", actual->n);
+		actual = actual->next;
+		len++;
+	}
+	return (len);
 }
