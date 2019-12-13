e264c7a4474c67d0ae567b6efae217eab1d6d64c
Alejo López <alejolo311@gmail.com>
2019-12-13 08:47:36 -0500

    measure the list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,19 @@
 #include "lists.h"
 
 /**
- * dlistint_len -
- *
- * Return: 
+ * dlistint_len - return the len of a double list
+ * @h: the double linked list
+ * Return: the len of the lists
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+	const dlistint_t *actual = h;
+	size_t len = 0;
 
+	while (actual)
+	{
+		actual = actual->next;
+		len++;
+	}
+	return (len);
 }
