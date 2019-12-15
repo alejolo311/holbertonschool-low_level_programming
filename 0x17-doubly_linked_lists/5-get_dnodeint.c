a06006054ce39afc3b5d5b94e87100efb44a8707
Alejo López <alejolo311@gmail.com>
2019-12-15 17:36:00 +0000

    found a node in a index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,11 +1,22 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index -
- *
- * Return: 
+ * get_dnodeint_at_index - get a specific index
+ * @head: the list.
+ * @index: the node to found
+ * Return: the node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	dlistint_t *actual;
 
+	actual	= head;
+	while (actual  != NULL && index != 0)
+	{
+		index--;
+		actual = actual->next;
+	}
+	if (actual != NULL)
+		return (actual);
+	return (NULL);
 }
