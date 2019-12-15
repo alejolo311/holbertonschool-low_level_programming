9d1cfeaf0f20a384d42fc13940ce63efd992ab97
Alejo López <alejolo311@gmail.com>
2019-12-15 18:29:11 +0000

    sum

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,21 @@
 #include "lists.h"
 
 /**
- * sum_dlistint -
- *
- * Return: 
+ * sum_dlistint - sum a list
+ * @head: the list
+ * Return: the sm
  */
 int sum_dlistint(dlistint_t *head)
 {
+		int result = 0;
+		dlistint_t *actual;
 
+		actual = head;
+
+		while (actual)
+		{
+			result += actual->n;
+			actual = actual->next;
+		}
+		return (result);
 }
