37158411a92706d6c162d6096e5e215fe3b412a2
Alejo López <alejolo311@gmail.com>
2019-12-17 04:08:14 +0000

    whaaaaaa!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/alejolo311/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/whaisthis.so
-export LD_PRELOAD=/tmp/whatisthis.so
+export LD_PRELOAD=/tmp/whaisthis.so
