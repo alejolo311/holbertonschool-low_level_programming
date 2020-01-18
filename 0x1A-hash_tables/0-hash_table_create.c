efe6f19383b1ad0107b8d93eb990aa51c3e4286d
alejolo311 <alejolo311@gmail.com>
2020-01-18 15:20:25 +0000

    create a table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,25 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create -
- *
- * Return: 
+ * hash_table_create - create a table
+ * @size: the size
+ * Return: a hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *hashTable;
 
+	if (size == 0)
+		return (NULL);
+	hashTable = calloc(1, sizeof(hash_table_t));
+	if (hashTable == NULL)
+		return (NULL);
+	hashTable->size = size;
+	hashTable->array = calloc(size, sizeof(hash_node_t **));
+	if (hashTable->array == NULL)
+	{
+		free(hashTable);
+		return (NULL);
+	}
+	return (hashTable);
 }
