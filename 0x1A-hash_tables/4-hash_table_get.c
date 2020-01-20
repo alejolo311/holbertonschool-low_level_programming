0cfc6f7c9e36eb338f150ad016bc7fdea6d21221
alejolo311 <alejolo311@gmail.com>
2020-01-20 03:19:26 +0000

    fix betty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,18 +1,16 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_get - returns a element
- * @ht: input 
+ * @ht: input
  * @key: key
  * Return: value of a element
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	
 	hash_node_t *actual = NULL;
-    unsigned long int index;
+	unsigned long int index;
 
-	if (ht == NULL|| *key == 0 || key == NULL)
+	if (ht == NULL || *key == 0 || key == NULL)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	actual = ht->array[index];
