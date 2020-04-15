a0f5f69e2c4f80b6bf82fa20ee1c414b47706fd8
alejolo311 <alejolo311@gmail.com>
2020-04-15 12:49:36 -0500

    make an algo to do linear search in an pointer of ints

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include "search_algos.h"
+/**
+ * linear_search - search for a number in a linear way
+ * @array: the input
+ * @size: the size of the input
+ * @value: the value to seaach
+ * Return: the index of the value, or -1 when the values is not present
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < (int )size; i++)
+	{
+		printf("Value checked array[%i] = [%i]\n", i,*(array + i));
+		if (*(array + i) == value)
+			return (i);
+	}
+	return (-1);
+}
