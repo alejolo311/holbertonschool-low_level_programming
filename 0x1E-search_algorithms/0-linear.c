d2f5169df865c42775d743e9b892dd96c8ff6dce
alejolo311 <alejolo311@gmail.com>
2020-04-15 14:34:26 -0500

    fix betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,9 +12,9 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	for (i = 0; i < (int )size; i++)
+	for (i = 0; i < (int)size; i++)
 	{
-		printf("Value checked array[%i] = [%i]\n", i,*(array + i));
+		printf("Value checked array[%i] = [%i]\n", i, *(array + i));
 		if (*(array + i) == value)
 			return (i);
 	}
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -19,10 +19,10 @@ int binary_search(int *array, size_t size, int value)
 	while (start <= end)
 	{
 		printf("Searching in array: ");
-	    for (i = start; i < end; i++)
-	    {
-		    printf("%d, ", *(array + i));
-	    }
+		for (i = start; i < end; i++)
+		{
+			printf("%d, ", *(array + i));
+		}
 	    printf("%d\n", *(array + i));
 		mid = (start + end) / 2;
 		if (value > *(array + mid))
