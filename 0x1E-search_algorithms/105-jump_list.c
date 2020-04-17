4e38fdb9cd86ddda2c19ad05d32beac9abce56d8
alejolo311 <alejolo311@gmail.com>
2020-04-16 23:11:39 -0500

    jump and linear in a linked list

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,52 @@
+ #include "search_algos.h"
+/**
+ * jump_list - search for a number in a jump again way
+ * @list: the input
+ * @size: the size
+ * @value: the value to seaach
+ * Return: the index of the value, or -1 when the values is not present
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	int s, e, st;
+	listint_t *actual, *p;
+
+	if (list == NULL)
+		return (NULL);
+    s = 0;
+    e = size - 1;
+    st = sqrt(size);
+	actual = list;
+	while (s < e)
+	{
+		s += st;
+		p = actual;
+		while (actual->next && (s != (int)actual->index))
+			actual = actual->next;
+		printf("Value checked at index [%li] = [%i]\n", actual->index, actual->n);
+		if (actual->n >= value)
+			return (linearSearch(p, actual, value));
+	}
+	return (linearSearch(p, actual, value));
+}
+/**
+ * linearSearch - search for a number in a linearSearch way
+ * @s: the input
+ * @e: the begining
+ * @value: the value to seaach
+ * Return: the index of the value, or -1 when the values is not present
+ */
+listint_t *linearSearch(listint_t *s, listint_t *e, int value)
+{
+	printf("Value found between indexes [%li] and [%li]\n",
+			s->index, e->index);
+	while (s != e->next)
+	{
+		printf("Value checked at index [%li] = [%i]\n",
+				s->index, s->n);
+		if (s->n == value)
+			return (s);
+		s = s->next;
+	}
+	return (NULL);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -14,5 +30,7 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int helperexp(int *array, int start, int end, int value);
 int helperBinary(int *array, int start, int end, int value);
+listint_t *linearSearch(listint_t *s, listint_t *e, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
